d924f33676cf0e57af89ab9e433b9c8593443d0c
cshlatz <cshlatz@gmail.com>
2018-11-27 21:11:22 -0500

    Launcher c lass

---
+++ Desktop/Login_System/src/Launcher.java
@@ -0,0 +1,24 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import Login_Sys.src.Login_System.*;
+
+public class Launcher {
+	private JFrame frame;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login_S window = new Login_S();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
